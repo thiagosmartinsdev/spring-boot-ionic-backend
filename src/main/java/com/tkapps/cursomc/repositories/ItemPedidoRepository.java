91c6284617639f69206f63aec43dd909e8e11f31
Thiago Martins <thiagomartinsseg@gmail.com>
2020-07-24 12:29:07 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/tkapps/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.tkapps.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tkapps.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{}
